6be55be6520c50c5a4988822336796d08c220f33
osmangony01 <mdosmangoni376@gmail.com>
2023-05-01 22:44:07 +0600

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Chef Recipe Hunter is running");
