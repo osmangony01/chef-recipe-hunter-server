d90767b992aecc4e2b24514eb6ca5ae7afddf151
osmangony01 <mdosmangoni376@gmail.com>
2023-05-03 19:32:10 +0600

    view chef and recipes in route perfectly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const chefs = require("./data/chefs.json")
+const chef_recipes = require("./data/chef_with_recipes.json");
 
 app.use(cors());
 
@@ -13,13 +13,13 @@ app.get("/", (req, res) => {
 })
 
 app.get("/chefs" ,(req, res) =>{
-    res.send(chefs);
+    res.send(chef_recipes);
 })
 
 app.get("/chef/:id", (req, res)=>{
     const id = req.params.id;
     console.log(id);
-    const selectedChef = chefs.find(c => c.id == id);
+    const selectedChef = chef_recipes.find(c => c.id == id);
     res.send(selectedChef);
 })
 
