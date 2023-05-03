e64ad35b9a23b96e7173950f9f7b4cd427b5f730
osmangony01 <mdosmangoni376@gmail.com>
2023-05-03 19:28:19 +0600

    create chef and recipe data perfectly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const chefes = require("./data/")
+const chefs = require("./data/chefs.json")
 
 app.use(cors());
 
@@ -12,6 +12,16 @@ app.get("/", (req, res) => {
     res.send("Chef Recipe Hunter is running");
 })
 
+app.get("/chefs" ,(req, res) =>{
+    res.send(chefs);
+})
+
+app.get("/chef/:id", (req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chefs.find(c => c.id == id);
+    res.send(selectedChef);
+})
 
 app.listen(port, () => {
     console.log(`Chef Recipe Hunter API is running on port: ${port}`);
